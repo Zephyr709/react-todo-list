d2c400eeb33eb3564996000912d8d202d6e99078
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2023-06-04 11:25:56 -0230

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const toggleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map((task) =>{
+      if (id === task.id) {
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    
+  }
+
+  const deleteTask = (id) => {
+    console.log(id);
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  };
+
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task,name: newName};
+      }
+      return task;
+    })
+    setTasks(editedTaskList);
+  };
+
+  const taskList = tasks.map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted} 
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
-
   ));
-  
+
+  const addTask = (name) => {
+    const newTask = {id: `todo-${nanoid()}` ,name , completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
